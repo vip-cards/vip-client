a1036187e68e5428eaae43da2bfdfaac1e83d270
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-12-09 16:49:27 +0200

    update search components

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import "./SearchInput.scss";
 import { ReactComponent as SearchIcon } from "../../assets/VIP-ICON-SVG/SearchIcon.svg";
 import { t } from "i18next";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStore } from "@fortawesome/free-solid-svg-icons";
 
-export default function SearchInput({ onChange }) {
+export default function SearchInput({ onChange, loading, ...props }) {
   return (
     <div className="form-search">
       <input
@@ -13,9 +15,13 @@ export default function SearchInput({ onChange }) {
         id="search"
         placeholder={t("Search")}
         onChange={onChange}
+        {...props}
       />
       <label className="label-search">
-        <SearchIcon className="SearchLogo" />
+        <div className={`search-icon-container ${loading ? "searching" : ""}`}>
+          <FontAwesomeIcon icon={faStore} className="search-icon-bg" />
+          <SearchIcon className="search-icon-mag" />
+        </div>
       </label>
     </div>
   );
