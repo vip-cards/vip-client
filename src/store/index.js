22df03f53531ac21dd5aa61ddaf470793e899b90
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-12-15 13:25:03 +0200

    add cart reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
+import { cartReducer } from "./cart-slice";
 import wishlistReducer from "./wishlist-slice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     wishlist: wishlistReducer,
+    cart: cartReducer
   },
 });
 
