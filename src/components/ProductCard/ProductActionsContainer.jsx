bc47cddd5d39d81ab67bce8cad6fe8c39ccd680c
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-12-17 15:55:52 +0200

    update product card

--- src/components/ProductCard/ProductActionsContainer.jsx
+++ src/components/ProductCard/ProductActionsContainer.jsx
@@ -1,7 +1,6 @@
 import { useState, forwardRef } from "react";
 import {
   faCircleNotch,
-  faHeart,
   faMinus,
   faPlus,
   faXmark,
@@ -15,22 +14,18 @@ import { addWishProduct } from "../../store/wishlist-slice";
 export const ProductActionsContainer = forwardRef(({ product }, ref) => {
   const lang = i18n.language;
   const dispatch = useDispatch();
+  const [popupLoading, setPopupLoading] = useState(false);
   const [addToCartState, setAddToCartState] = useState({
     active: false,
     count: 1,
     error: false,
   });
-
   const cartBranch = useSelector((state) => state.cart.branch);
   const wishlistIds = useSelector((state) => state.wishlist.ids);
-  console.log(cartBranch);
 
   function cofirmAddToCartStateHandler(branchId) {
-    console.log(branchId);
-    console.log(cartBranch);
     if (branchId !== cartBranch._id) {
-      setAddToCartState((state) => ({ ...state, error: true }));
-
+      setError(true);
       return;
     }
     dispatch(
@@ -40,35 +35,52 @@ export const ProductActionsContainer = forwardRef(({ product }, ref) => {
         quantity: addToCartState.count,
       })
     );
-    console.log("added" + product._id, "--->count" + addToCartState.count);
-    console.log(product);
-    console.log(branchId);
-    // setAddToCartState((state) => ({ ...state, count: 1, active: false }));
   }
 
+  const setError = (check) => {
+    setAddToCartState((state) => ({ ...state, error: !!check }));
+  };
+  const setActive = (check) => {
+    setAddToCartState((state) => ({ ...state, active: !!check }));
+  };
+  const changeCount = (fn) => {
+    setAddToCartState((state) => {
+      let newCount = 0;
+      if (fn === "plus") {
+        newCount = state.count + 1;
+      } else if (fn === "minus") {
+        newCount = state.count - 1;
+      }
+      return {
+        ...state,
+        count: newCount,
+      };
+    });
+  };
   const BranchSelectPopup = () => {
     return (
       <div className="branch-select-error">
         <p>You can only choose the branch in your cart</p>
         <div className="branch-select-error-actions">
-          <button
-            className="action-cancel"
-            onClick={() =>
-              setAddToCartState((state) => ({ ...state, error: false }))
-            }
-          >
+          <button className="action-cancel" onClick={() => setError(false)}>
             <FontAwesomeIcon icon={faXmark} />
           </button>
-          {console.log(product._id)}
-          {console.log(wishlistIds.includes(product._id))}
           {wishlistIds.includes(product._id) ? (
             <div className="already-whishlisted">already in whishlist</div>
           ) : (
             <button
               onClick={() => {
-                dispatch(addWishProduct(product._id)).then(() => {
-                  setTimeout(() => {}, 900);
-                });
+                setPopupLoading(true);
+                dispatch(addWishProduct(product._id))
+                  .unwrap()
+                  .then(() => {
+                    setAddToCartState((state) => ({
+                      ...state,
+                      active: false,
+                      error: false,
+                    }));
+                    setPopupLoading(false);
+                  });
               }}
             >
               add to wishlist
@@ -86,35 +98,16 @@ export const ProductActionsContainer = forwardRef(({ product }, ref) => {
         >
           <span
             className="add-field-minus"
-            onClick={() => {
-              if (addToCartState.count <= 1) return;
-              setAddToCartState((state) => ({
-                ...state,
-                count: state.count - 1,
-              }));
-            }}
+            onClick={() => addToCartState.count > 1 && changeCount("minus")}
           >
             <FontAwesomeIcon icon={faMinus} className="fa-xs" />
           </span>
           <span className="add-field-input">{addToCartState.count}</span>
-          <span
-            className="add-field-plus"
-            onClick={() => {
-              setAddToCartState((state) => ({
-                ...state,
-                count: state.count + 1,
-              }));
-            }}
-          >
+          <span className="add-field-plus" onClick={() => changeCount("plus")}>
             <FontAwesomeIcon icon={faPlus} className="fa-xs" />
           </span>
         </div>
-        <div
-          className="add-buttons"
-          onClick={() =>
-            setAddToCartState((state) => ({ ...state, active: true }))
-          }
-        >
+        <div className="add-buttons" onClick={() => setActive(true)}>
           <span>Add to cart</span>
         </div>
       </div>
@@ -123,31 +116,33 @@ export const ProductActionsContainer = forwardRef(({ product }, ref) => {
           className="branches-list"
           ref={ref}
           onAnimationEnd={(e) => {
-            console.log(e);
-            if (e.animationName === "branches-popup-close")
-              setAddToCartState((state) => ({ ...state, active: false }));
+            if (e.animationName === "branches-popup-close") setActive(false);
           }}
         >
-          <div className="loading-icon">
-
-          <FontAwesomeIcon icon={faCircleNotch} className="fa-spin" />
-          </div>
-          {product.branches?.map((branch) => {
-            return (
-              <div
-                key={branch._id}
-                className={`branch-item ${
-                  branch._id !== cartBranch._id ? "disabled" : null
-                }`}
-                onClick={() => cofirmAddToCartStateHandler(branch._id)}
-              >
-                {branch.name[lang]}
-              </div>
-            );
-          })}
+          {popupLoading ? (
+            <div className="loading-icon">
+              <FontAwesomeIcon icon={faCircleNotch} className="fa-spin" />
+            </div>
+          ) : (
+            <>
+              {product.branches?.map((branch) => {
+                return (
+                  <div
+                    key={branch._id}
+                    className={`branch-item ${
+                      branch._id !== cartBranch._id ? "disabled" : null
+                    }`}
+                    onClick={() => cofirmAddToCartStateHandler(branch._id)}
+                  >
+                    {branch.name[lang]}
+                  </div>
+                );
+              })}
+            </>
+          )}
         </div>
       ) : null}
-      {addToCartState.active && addToCartState.error && <BranchSelectPopup />}{" "}
+      {addToCartState.active && addToCartState.error && <BranchSelectPopup />}
     </>
   );
 });
