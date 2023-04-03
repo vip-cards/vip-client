63602bf07444c47dd2d17a580d495201156353c3
Mohammed Khallaf <mohamed.khallaf@xpence.uk>
2023-04-03 10:11:27 +0200

    add isActive params to get request

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -14,10 +14,7 @@ Axios.defaults.baseURL = endPoint + "/client";
 Axios.defaults.headers["x-app-token"] = "VIP-Team";
 
 Axios.interceptors.request.use(async (req) => {
-  if (guestEndpoints.includes(req.url)) {
-
-    return req;
-  }
+  if (guestEndpoints.includes(req.url)) return req;
 
   let auth = store.getState().auth;
 
@@ -27,10 +24,11 @@ Axios.interceptors.request.use(async (req) => {
 
   if (!isTokenExpired) {
     req.headers.Authorization = `Bearer ${auth.token}`;
+    if (req.method === "get") {
+      req.params = { ...req.params, isActive: true };
+    }
     return req;
-  }
-
-  if (isTokenExpired) {
+  } else {
     localStorage.removeItem("token");
     store.dispatch(authActions.logout());
 
