718455660f4ef55028942dba5b97358c33d9c486
Mohammed Khallaf <mohammed.khallaf.1997@gmail.com>
2023-04-08 16:28:12 +0200

    update services page

---
+++ src/components/LoadingSkeleton/LoadingSkeleton.jsx
@@ -0,0 +1,6 @@
+export const LoadingSkeleton = ({ width = "16rem", height = "1.7rem" }) => (
+  <div
+    className="animate-pulse bg-primary/20 rounded-lg"
+    style={{ width, height }}
+  ></div>
+);
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,13 +1,135 @@
+import CardContainer from "components/CardContainer/CardContainer";
+import { LoadingSkeleton } from "components/LoadingSkeleton/LoadingSkeleton";
+import Pagination from "components/Pagination/Pagination";
 import Tabs from "components/Tabs/Tabs";
+import dayjs from "dayjs";
 import { getLocalizedWord } from "helpers/lang";
 import { listRenderFn } from "helpers/rednerFn";
 import { useLayoutEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import clientServices from "services/clientServices";
 import useSWR from "swr";
 import "./Services.scss";
-import Pagination from "components/Pagination/Pagination";
 
-const LIMIT = 2;
+const LIMIT = 9;
+
+export function ServiceDetails() {
+  const { id } = useParams();
+  const {
+    data: service,
+    error,
+    isLoading,
+    isValidating,
+    mutate,
+  } = useSWR(
+    `${id}-service`,
+    async () => (await clientServices.getService(id))?.record[0]
+  );
+
+  if (isLoading)
+    return (
+      <CardContainer title="Service Provider">
+        <div className="flex flex-col divide-y-2 gap-4">
+          <div className="flex flex-col border-b-slate-200">
+            <h5>Service</h5>
+            <p className="text-black/90">
+              <LoadingSkeleton />
+            </p>
+          </div>
+          <div className="flex flex-col border-b-slate-200">
+            <h5>Description</h5>
+            <p className="text-black/90 max-w-[80%]">
+              <LoadingSkeleton />
+            </p>
+          </div>
+          <div className="flex flex-col border-b-slate-200">
+            <h5>Contacts</h5>
+            <div className="flex flex-col gap-2 ml-1 w-fit">
+              <div>
+                <h6>Phone</h6>
+                <p className="text-black/90">
+                  <LoadingSkeleton width="10rem" />
+                </p>
+              </div>
+              <div>
+                <h6>Whatsapp</h6>
+                <p className="text-black/90">
+                  <LoadingSkeleton width="10rem" />
+                </p>
+              </div>
+              <div>
+                <h6>Telegram</h6>
+                <p className="text-black/90">
+                  <LoadingSkeleton width="10rem" />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col border-b-slate-200">
+            <h5>Address</h5>
+            <p className="text-black/90">
+              {" "}
+              <LoadingSkeleton />
+            </p>
+          </div>
+          <div className="flex flex-col border-b-slate-200">
+            <h5>Published</h5>
+            <p className="text-black/90">
+              <LoadingSkeleton />
+            </p>
+          </div>
+        </div>
+      </CardContainer>
+    );
+
+  return (
+    <CardContainer title={getLocalizedWord(service.providerName)}>
+      <div className="flex flex-col divide-y-2 gap-4">
+        <div className="flex flex-col border-b-slate-200">
+          <h5>Service</h5>
+          <p className="text-black/90">
+            {getLocalizedWord(service.serviceName)}
+          </p>
+        </div>
+        <div className="flex flex-col border-b-slate-200">
+          <h5>Description</h5>
+          <p className="text-black/90 max-w-[80%]">
+            {getLocalizedWord(service.description)}
+          </p>
+        </div>
+        <div className="flex flex-col border-b-slate-200">
+          <h5>Contacts</h5>
+          <div className="flex flex-col gap-2 ml-1 w-fit">
+            <div>
+              <h6>Phone</h6>
+              <p className="text-black/90">{service.contacts.phone ?? ""}</p>
+            </div>
+            <div>
+              <h6>Whatsapp</h6>
+              <p className="text-black/90">{service.contacts.whatsapp ?? ""}</p>
+            </div>
+            <div>
+              <h6>Telegram</h6>
+              <p className="text-black/90">{service.contacts.telegram ?? ""}</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col border-b-slate-200">
+          <h5>Address</h5>
+          <p className="text-black/90">{getLocalizedWord(service.address)}</p>
+        </div>
+        <div className="flex flex-col border-b-slate-200">
+          <h5>Published</h5>
+          <p className="text-black/90">
+            {dayjs(service.publishDate).format("DD-MMMM-YYYY")}
+          </p>
+        </div>
+      </div>
+    </CardContainer>
+  );
+}
+
 function ServiceHome({ id = undefined }) {
   const initialQueryParams = { page: 1, limit: LIMIT };
   const [queryParams, setQueryParams] = useState(initialQueryParams);
@@ -23,16 +145,19 @@ function ServiceHome({ id = undefined }) {
     list: services,
     render: (service) => {
       return (
-        <div
+        <Link
           key={service._id}
-          className="w-64 bg-primary/20 hover:bg-primary/70 transition-colors shadow rounded-lg p-5 flex flex-col gap-3"
+          to={`/services/${service._id}`}
+          className="text-left w-64 bg-primary/20 hover:bg-primary/70 transition-colors shadow rounded-lg p-5 flex flex-col gap-3"
         >
-          <h5>{getLocalizedWord(service.providerName)}</h5>
+          <h5 className="whitespace-nowrap overflow-x-hidden text-ellipsis">
+            {getLocalizedWord(service.providerName)}
+          </h5>
           <p className="font-semibold">
             {getLocalizedWord(service.serviceName)}
           </p>
           <p>{getLocalizedWord(service.description)}</p>
-        </div>
+        </Link>
       );
     },
   };
@@ -75,7 +200,7 @@ export default function Services() {
   };
 
   return (
-    <div className="page-wrapper app-card-shadow px-0 md:px-5 lg:px-8 py-8 m-8">
+    <div className="page-wrapper app-card-shadow px-0 md:px-4 lg:px-6 py-8 m-8">
       <Tabs tabs={tabs} />
     </div>
   );
